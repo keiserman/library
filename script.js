2ecffe9d2253028a69bd97540d8c1bf36decd282
Kevin <eisermanka@gmail.com>
2023-12-03 13:10:01 -0500

    Added remove book function

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 const BOOK_GRID = document.querySelector(".book-grid");
 const BOOK_FORM = document.querySelector("form");
-const myLibrary = [];
+const MODAL = document.querySelector("dialog");
+const OPEN_BUTTON = document.querySelector("#openModal");
+const CLOSE_BUTTON = document.querySelector("#closeModal");
 
-const dialog = document.querySelector("dialog");
-const showButton = document.querySelector("#openModal");
-const closeButton = document.querySelector("#closeModal");
-
-showButton.addEventListener("click", () => {
-  dialog.showModal();
-});
-
-closeButton.addEventListener("click", () => {
-  dialog.close();
-});
+const myLibrary = [
+  {
+    title: "Harry Potter",
+    author: "J.K. Rowling",
+    pages: 234,
+    read: "yes",
+  },
+  {
+    title: "The Hobbit",
+    author: "J.R.R. Tolkien",
+    pages: 320,
+    read: "yes",
+  },
+];
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -21,12 +26,20 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
+OPEN_BUTTON.addEventListener("click", () => {
+  MODAL.showModal();
+});
+
+CLOSE_BUTTON.addEventListener("click", () => {
+  MODAL.close();
+});
+
 function updateLibrary(library) {
   resetGrid();
-  library.forEach((book) => {
+  library.forEach((book, index) => {
     const bookCard = document.createElement("div");
     bookCard.innerHTML = `
-      <div class="card">
+      <div class="card" data-id="${index}">
         <div class="card-header">
           <p class="book-title">${book.title}</p>
         </div>
@@ -34,10 +47,15 @@ function updateLibrary(library) {
           <p><strong>Author:</strong> <span class="book-author">${book.author}</span></p>
           <p><strong>Pages:</strong> <span class="book-pages">${book.pages}</span></p>
           <p><strong>Read:</strong> <span class="book-read">${book.read}</span></p>
-          <button class="btn btn-secondary">Remove</button>
+          <button id="removeBook" class="btn btn-secondary">Remove</button>
         </div>
       </div>
     `;
+    bookCard.addEventListener("click", (event) => {
+      if (event.target.matches("#removeBook")) {
+        removeBook(index, library);
+      }
+    });
     BOOK_GRID.appendChild(bookCard);
   });
 }
@@ -46,12 +64,15 @@ function resetGrid() {
   BOOK_GRID.innerHTML = "";
 }
 
-function addBook(title, author, pages, read) {
-  let book = new Book(title, author, pages, read);
-  myLibrary.push(book);
+function addBook(book, library) {
+  library.push(book);
+  updateLibrary(library);
 }
 
-function removeBook() {}
+function removeBook(index, library) {
+  library.splice(index, 1);
+  updateLibrary(library);
+}
 
 BOOK_FORM.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -62,7 +83,10 @@ BOOK_FORM.addEventListener("submit", (event) => {
   const pages = formData.get("pages");
   const read = formData.get("read");
 
-  addBook(title, author, pages, read);
-  updateLibrary(myLibrary);
+  let book = new Book(title, author, pages, read);
+
+  addBook(book, myLibrary);
   BOOK_FORM.reset();
 });
+
+updateLibrary(myLibrary);
